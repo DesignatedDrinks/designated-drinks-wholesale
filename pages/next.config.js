a91cf4f1699381a77d6651de36a42e98971c21c8
DesignatedDrinks <designateddrinksonline@gmail.com>
2025-02-25 10:15:55 -0500

    Update next.config.js

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", 
+  basePath: "/designated-drinks-wholesale", // Your GitHub repo name
+  assetPrefix: "/designated-drinks-wholesale/",
+  images: {
+    unoptimized: true, // Ensures images work on GitHub Pages
+  },
 };
+
+module.exports = nextConfig;
